b987f9661d519626bfa987c9ab858dd12bcb67bb
ahmedakhtar11 <ahmedakhtar11@yahoo.com>
2019-06-17 23:04:58 -0500

    App Set Up

---
+++ todolist-vue.js/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <Todos />
+  </div>
+</template>
+
+<script>
+
+import Todos from './components/Todos';
+
+export default {
+  name: 'app',
+  components: {
+    Todos
+  }
+,
+  data() {
+    return {
+      todos:[
+        {
+          id: 1,
+          title: "Todo One",
+          completed: false
+        },
+               {
+          id: 1,
+          title: "Todo One",
+          completed: false
+        },
+               {
+          id: 1,
+          title: "Todo One",
+          completed: false
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+* {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+}
+
+</style>
