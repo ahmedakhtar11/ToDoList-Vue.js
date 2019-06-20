e3391e927738146d6343899002125a4d0567b05d
ahmedakhtar11 <ahmedakhtar11@yahoo.com>
2019-06-19 23:48:26 -0500

    Adding Delete Button

--- todolist-vue.js/src/components/Todos.vue
+++ todolist-vue.js/src/components/Todos.vue
@@ -1,8 +1,9 @@
 <template>
     <div>
         <h1>Todos</h1>
+        
             <div v-bind:key="todo.id" v-for="todo in todos">
-                <TodoItem />
+                <TodoItem v-bind:todo="todo" v-on:del-todo="$emit('del-todo', todo.id)"/>
         </div>
     </div>
 </template>
